ee641cc13d01246eb4f4ba0d2443c6c5afb8a6e7
anshu <manu22425@gmail.com>
2024-04-06 02:44:23 +0530

    Minor fixes in Login and started SignUp

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,14 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { signIn } from '../utils/redux/userSlice';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [signInMode, setSignInMode] = useState(true);
+  const [validationError, setValidationError]  = useState(null);
   const email = useRef(null);
   const password = useRef(null);
+  const username = useRef(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -27,24 +30,44 @@ const Login = () => {
     };
 
     fetch("http://localhost:5000/auth/signInUser", requestOptions)
-      .then((response) => response.text())
-      .then((result) => {
-        dispatch(signIn(result));
-        navigate("/home");
+      .then((response) => {
+        return Promise.all([response.status, response.json()]);
       })
-      .catch((error) => console.error(error));
+      .then(([status, result]) => {
+        console.log(result);
+        console.log(status);
+        if(status==200){
+          dispatch(signIn(result.authToken));
+          return navigate("/home");
+        }
+        else{
+          throw new Error(result.errors)
+        }
+        
+      })
+      .catch((error) => {
+        alert(error);
+        navigate('/');
+      });
+
   }
   return (
     <div className='bg-gradient-to-r from-indigo-500  to-pink-500 w-1/3 ms-auto me-auto py-5 ps-4 pe-4 mt-24 shadow-inner'>
-      <p className='text-center font-bold text-xl mb-2'>Login</p>
+      <p className='text-center font-bold text-xl mb-2'>{signInMode ? "Login" : "SignUp"}</p>
       <form onSubmit={handleSubmit}>
+        {!signInMode && (
+          <>
+            <label htmlFor='username'>Username</label>
+            <input type="text" id="username" name="username" className='border w-full bg-black bg-opacity-60 p-1 my-2' ref={username} />
+          </>
+        )}
         <label htmlFor="email">Email:</label><br />
         <input type="text" id="email" name="email" className='border w-full bg-black bg-opacity-60 p-1 my-2' ref={email} />
         <label htmlFor="password">Password:</label><br />
         <input type="password" id="password" name="password" className='border w-full bg-black bg-opacity-60 p-1 my-2' ref={password} />
         <div className='mt-4 text-center'>
-          <button type='submit' className='p-2 bg-red-600 m-1 border-white rounded-md'>Login</button>
-          <button type='submit' className='p-2 bg-yellow-500 m-1 border-white rounded-md text-black'>SignUp</button>
+          <button type='submit' className='p-2 bg-red-600 m-1 border-white rounded-md'>{signInMode ? "Login" : "SignUp"}</button>
+          <button type='button' className='p-2 bg-yellow-500 m-1 border-white rounded-md text-black' onClick={()=>setSignInMode(!signInMode)}>{signInMode ? "Don't have an account?" : "Go back to Login"}</button>
         </div>
       </form>
     </div>
--- src/utils/redux/userSlice.js
+++ src/utils/redux/userSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: "user",
     initialState: {
-        user: "abra"
+        user: null
     },
     reducers: {
         signIn: (state, action) => {
