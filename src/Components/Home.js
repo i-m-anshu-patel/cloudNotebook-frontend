45cee514c23b430f042a89c0cade4652df938544
anshu <manu22425@gmail.com>
2024-04-09 01:56:56 +0530

    code optimized

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,44 +6,35 @@ const Home = () => {
   const records = sample.notes;
   const [previousNotes, setPreviousNotes] = useState(records);
   const [notes, setNotes] = useState(records);
-  const [generalTag, setGeneralTag] = useState(false);
-  const [specialTag, setSpecialTag] = useState(false);
+  const [tagFilter, setTagFilter] = useState(null);
+  const tagNames = [...new Set(previousNotes.map(notes => notes.tag))];
  
-  useEffect(()=>{
-    if(generalTag){
-      const newNotes = notes.filter((note) => note.tag === 'General');
-      setNotes(newNotes);
+  const handleTagFilter = (tagName) => {
+    if(tagName === 'clear'){
+      setNotes(previousNotes);
+      setTagFilter(null);
     }
     else{
-      setNotes(previousNotes);
-    }
-  }, [generalTag]);
-
-
-  useEffect(()=>{
-    if(specialTag){
-      const newNotes = notes.filter((note) => note.tag === 'Special');
+      const newNotes = previousNotes.filter((note) => note.tag === tagName);
       setNotes(newNotes);
+      setTagFilter(tagName);
     }
-    else{
-      setNotes(previousNotes);
-    }
-  }, [specialTag])
+  }
 
   return (
     <div className='mt-5'>
       <div className='flex my-5 ms-8'>
         <div className='ms-auto me-3'>
-        <button type='button' className={`mx-2 rounded-xl px-2 py-1 border border-gray-500 hover:border-white`} onClick={() => setSpecialTag(!specialTag)}>Special</button>
-        <button type='button' className={`mx-2 rounded-xl px-2 py-1 border border-gray-500 hover:border-white`} onClick={() => setGeneralTag(!generalTag)}>General</button>
-          <button type='button' className='mx-2 rounded-xl px-2 py-1 border border-gray-500 hover:border-white'>Sort by earliest</button>
+          {tagNames && tagNames.map((tag, index) => (
+             <button key={index} type='button' className={`mx-2 rounded-xl px-2 py-1 border border-gray-500 hover:border-white bg-${tagFilter === tag ? 'black border-white' : 'transparent'}`} onClick={() => handleTagFilter(tag)}>{tag}</button>
+          ))}
+          {tagFilter && 
+          (<button type='button' className='bg-red-500 mx-2 px-2 py-1 rounded' onClick={() => handleTagFilter('clear')}>Clear All Filters</button>)
+          }
           <button type='button' className='bg-blue-500 mx-2 px-2 py-1 rounded'>Create Note</button>
           <input type='text' className='rounded py-1 px-1' placeholder='Search' />
         </div>
       </div>
-
-
-
       <div className='grid grid-cols-4 gap-4'>
         {notes && notes.map((record) =>
           (<NoteCard key={record._id} note={record} />)
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -7,7 +7,7 @@ const NoteCard = ({note}) => {
             <div className="p-4">
                 <h2 className="font-bold text-xl mb-2 text-black">{note.title}</h2>
                 <p className="text-gray-700">{note.description}</p>
-                <button type='button'className='mt-3 mx-1  text-gray-700'>{note.tag}</button>
+                <button type='button'className='mt-3 mx-1 px-1 py-1 rounded-xl text-gray-700 border border-gray-500'>#{note.tag}</button>
                 <div className="mt-4">
                    <button type='button' className='bg-yellow-500 rounded-md px-2 py-1 mx-1'>Edit</button>
                    <button type='button' className='bg-red-500 rounded-md px-2 py-1 mx-1'>Delete</button>
